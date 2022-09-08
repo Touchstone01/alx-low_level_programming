6acf4a42318cee682b3af97d5823192738f7f817
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-08 13:52:26 -0700

    First Write

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #!/bin/bash
-put(Programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
+
