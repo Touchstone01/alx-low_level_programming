34bc302a71c647513600034d74be6ed88902c395
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-09 12:14:12 -0700

    First If

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+int main (void)
+{
+	int n;
+	srand(time(0));
+	n=rand(0) - RAND_MAX / 2;
+	if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if(n<0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return 0;
+}
