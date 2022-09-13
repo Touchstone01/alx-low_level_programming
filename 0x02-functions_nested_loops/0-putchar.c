953ee0b421fa39f00fbe04c1edab4cfce91322e9
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-13 15:39:56 -0700

    Hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,11 @@
 #include "main.h"
+
 /**
- *
- *  main - prints Best School
- 
- *  Description: prints Best School
- *
- *Return: Always 0 (Success))
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
-<<<<<<< HEAD
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -23,18 +16,7 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-
-=======
-	for (int i=0; i = 0; i < 9; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-
->>>>>>> f06c91c0dd2abafdcc55ffdd7a305137c52ececa
 	return (0);
-
-
 }
 
 
