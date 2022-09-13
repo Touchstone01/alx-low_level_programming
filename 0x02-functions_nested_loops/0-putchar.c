5483a43a88c7c639c9c94bd1fdc29dcf7946cd64
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-13 12:42:57 -0700

    Hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -18,11 +18,10 @@ int main(void)
 
 	char text[11] = "Best School";
 
-	int i = 0;
-	for (i = 0; i < 9; i++)
-		_putchar(text[i]);
 
-	_putchar('\n');
+	for (int i = 0; i < 9; i++)
+		_putchar(text[i]);
+		_putchar('\n');
 
 	return (0);
 
