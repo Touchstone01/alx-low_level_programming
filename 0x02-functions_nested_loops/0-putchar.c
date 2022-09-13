279383c868babe5cedb10d01948c36778339eeeb
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-13 10:16:26 -0700

    Hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 
-int main (void)
+int main(void)
 {
-	printf("_putchar");
-	return 0;
+	char word[11] = "Best School";
+
+	int i;
+	for (i = 0; i < 9; i++)
+
+		_putchar(word[i]);
+
+	_putchar('\n');
+
+
+
+	return (0);
 }
