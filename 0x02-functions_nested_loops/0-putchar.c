8132b1afe2b37352ccd75e4de3d0b117c673a9a4
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-13 12:12:13 -0700

    Hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,61 @@
-#include <main.h>
+nclude "holberton.h"
+
+
+
+/**
+ *
+ *  * main - prints Holberton
+ *
+ *   *
+ *
+ *    * Description: prints Holberton
+ *
+ *     *
+ *
+ *      * Return: 0 (Success)
+ *
+ *       */
+
+
 
 int main(void)
+
 {
-	char word[11] = "Best School";
 
-	int i;
-	for (i = 0; i < 9; i++)
+		char text[10] = "Holberton";
 
-		_putchar(word[i]);
-
-	_putchar('\n');
+			int i = 0;
 
 
 
-	return (0);
+				for (i = 0; i < 9; i++)
+
+						{
+
+									_putchar(text[i]);
+
+										}
+
+					_putchar('\n');
+
+
+
+						return (0);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
