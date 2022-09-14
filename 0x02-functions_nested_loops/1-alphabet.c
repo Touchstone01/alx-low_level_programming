6a980b72685fee99ea46abc3a70690a7deabf27a
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-13 18:57:26 -0700

    Hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,35 +1,21 @@
 #include "main.h"
 
-<<<<<<< HEAD
-
-
-/*
- *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- */
-void print_alphabet(void)
-
-{
-
-	char i = 'a';
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
-=======
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_alphabet - Entry point
+ * Return:0
  */
-int main(void)
+
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
->>>>>>> ee781bc240f446a7fbf5ebc0a4b7495450ec4c1b
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
 }
 
