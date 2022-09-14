688b2311c4999dbf1039c1ec1e994fc120293beb
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-13 18:25:11 -0700

    Hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 
 
-/**
+/*
  *
  *  * main - check the code
  *
@@ -10,13 +10,13 @@
  *
  *    * Return: Always 0.
  *
- *     */
+ */
 void print_alphabet(void)
 
 {
 
 	char i = 'a';
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
