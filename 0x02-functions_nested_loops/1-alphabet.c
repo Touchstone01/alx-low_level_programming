2ae63bef3a308fc1fbed4af49216248dbcb8985c
Lateefat Shuaib <ayomidelateefat23@gmail.com>
2022-09-13 17:09:10 -0700

    hunm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,7 +11,19 @@
  *    * Return: Always 0.
  *
  *     */
+void print_alphabet(void)
 
+{
+
+	char i = 'a';
+	for(i='a'; i<'z'; i++)
+	{
+	_putchar(i);
+
+	}
+
+	_putchar('\n');
+}
 int main(void)
 
 {
