bf110247de9cbcfa18b7b532a0416203a1a83af8
[Lateefat Shuaib] <[ayomidelateefat23@gmail.com]>
2022-09-19 18:21:11 +0100

    Hello

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,13 @@
  * int_strlen - A function that returens the length
  * of a string
  * @s: string
- * Return: void 
+ * Return:void 
  *
 */
 int _strlen(char *s)
 {
 	int i = 0;
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
