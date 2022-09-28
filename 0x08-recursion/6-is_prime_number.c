9443e9298521b057fef87b1ccf6dfa018b264551
[Lateefat Shuaib] <[ayomidelateefat23@gmail.com]>
2022-09-28 21:10:55 +0100

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,7 @@
  * @m: comparison number
  * Return: 1 if not found sqrroot, else sqrroot
  **/
-int sqtRecursive(int n, int m)
+int sqtRecursive(int n, int m);
 {
 	if (n <= 0)
 		return (-1);
@@ -18,7 +18,7 @@ int sqtRecursive(int n, int m)
  * @n: given number
  * Return: square root of n or -1
  **/
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n);
 {
 	if (n == 1)
 		return (1);
@@ -29,10 +29,10 @@ int _sqrt_recursion(int n)
  * @n: given number
  * Return: 1 if number is prime else 0
  **/
-int is_prime_number(int n)
+int is_prime_number(int n);
 {
 	if (n <= 1 || _sqrt_recursion(n) >= 1)
 		return (0);
 	if (_sqrt_recursion(n) == -1)
 		return (1);
-
+}
