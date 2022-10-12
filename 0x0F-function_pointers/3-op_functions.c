e133da91a889b678a2059bb62fec5fd99f17648b
[Lateefat Shuaib] <[ayomidelateefat23@gmail.com]>
2022-10-12 23:01:59 +0100

    Hello

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,55 +1,30 @@
 #include "3-calc.h"
+
 /**
- * op_add - adds two ints
- * @a: number 1
- * @b: number 2
- * Return: sum of the two numbers
- **/
-int op_add(int a, int b)
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user.
+ * @s: char operator.
+ *
+ * Return: pointer to the function that corresponds to the operator.
+ */
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-/**
- * op_sub - subtract two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of substraction
- **/
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - multiply two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of multiplication
- **/
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - divides two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of divison
- **/
-int op_div(int a, int b)
-{
-	if (b == 0)
-		return (-1);
-	return (a / b);
-}
-/**
- * op_mod - calculates mod of two ints
- * @a: number 1
- * @b: number 2
- * Return: mod of 1, 2
- **/
-int op_mod(int a, int b)
-{
-	if (b == 0)
-		return (-1);
-	return (a % b);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
+
+	return (ops[i / 2].f);
 }
